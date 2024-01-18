284eba263e5410a50a60130577ff4a3728ef8e1c
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-01-18 22:38:52 +0100

    added two way binding to input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,21 +2,26 @@ import { useState } from "react";
 
 export default function Player({name, symbol}) {
     const [isEditing,setIsEditing]=useState(false);
+    const [playerName,setPlayerName]=useState(name);
 
     function editPlayer(){
-        setIsEditing(true);
+        setIsEditing(!isEditing);
+    }
+
+    function handleChange(event){
+        setPlayerName(event.target.value);
     }
   return (
     <>
       
           <li>
             <span className="Player">
-            {!isEditing && <span className="player-name">{name}</span>}
-            {isEditing && <input type="text" />}
+            {!isEditing && <span className="player-name">{playerName}</span>}
+            {isEditing && <input type="text" required value={playerName} onChange={handleChange}/>}
             <span className="player-symbol">{symbol}</span>
             </span>
           </li>
-       <button onClick={editPlayer}>Edit</button>
+       <button onClick={editPlayer}>{ isEditing ? "Save":"Edit"}</button>
     </>
   );
 }
