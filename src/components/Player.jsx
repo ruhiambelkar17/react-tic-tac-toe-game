9e07e62f77026d3dd6fdd08037fe90aec32b9a3a
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-01-18 15:43:49 +0100

    added edit functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
+
 export default function Player({name, symbol}) {
+    const [isEditing,setIsEditing]=useState(false);
+
+    function editPlayer(){
+        setIsEditing(true);
+    }
   return (
     <>
       
           <li>
             <span className="Player">
-            <span className="player-name">{name}</span>
+            {!isEditing && <span className="player-name">{name}</span>}
+            {isEditing && <input type="text" />}
             <span className="player-symbol">{symbol}</span>
             </span>
           </li>
-       <button>Edit</button>
+       <button onClick={editPlayer}>Edit</button>
     </>
   );
 }
