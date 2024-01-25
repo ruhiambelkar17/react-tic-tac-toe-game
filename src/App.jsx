dff864d264a74e81ef0c674757b8a04aa4679d76
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2024-01-25 18:08:56 +0100

    added game restart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
 
   const activePlayer=deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map((array)=>[...array])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -59,6 +59,10 @@ function App() {
     })
   }
 
+  function handleRestart(){
+    setGameTurns([]);
+  }
+
   return (
     <>
       <main>
@@ -67,7 +71,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}></Player>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer==='O'}></Player>
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner}></GameOver>}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}></GameOver>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}></GameBoard>
       </div>
       <Log turns={gameTurns}></Log>
